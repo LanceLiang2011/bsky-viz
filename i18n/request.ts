2e1214d2e87e798a174b4eeab06cee681e388992
LanceLiang2011 <liangxianyi2011@gmail.com>
2025-05-27 21:40:27 -0400

    fix bug

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,13 +1,12 @@
 import { getRequestConfig } from "next-intl/server";
-import { routing } from "./routing";
 
 export default getRequestConfig(async ({ requestLocale }) => {
   // This typically corresponds to the `[locale]` segment
   let locale = await requestLocale;
 
   // Ensure that a valid locale is used
-  if (!locale || !routing.locales.includes(locale as any)) {
-    locale = routing.defaultLocale;
+  if (!locale || !["en", "zh-cn"].includes(locale)) {
+    locale = "en"; // default locale
   }
 
   return {

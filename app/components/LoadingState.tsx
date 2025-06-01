57a29799ee7be0ca92fe10271fcd888cadf7cd0f
LanceLiang2011 <liangxianyi2011@gmail.com>
2025-06-01 16:33:57 -0400

    content: add more animation

--- app/components/LoadingState.tsx
+++ app/components/LoadingState.tsx
@@ -21,6 +21,10 @@ const animations = [
   "https://lottie.host/fe1e8db3-cafd-452b-9a08-5eb75d8f9abd/MuGlhMw0bk.lottie",
   "https://lottie.host/1a64fe0e-1d30-4938-92ac-5c62d0b34434/zXGgXwzSG9.lottie",
   "https://lottie.host/789ba419-1f71-4831-8d34-c7c36f2f107a/7bdEnG4NLw.lottie",
+  "https://lottie.host/7185eb3a-f679-481b-af5d-1d28446496d6/qOarOIS2d0.lottie",
+  "https://lottie.host/10033523-da5d-4f17-8074-a9ceb9a00b8f/ICxXy91Jyg.lottie",
+  "https://lottie.host/7cf2449e-4a12-43f3-928d-b4c9380909be/Fl4090Tyo1.lottie",
+  "https://lottie.host/edea6eef-e18a-4c19-9c37-0ce5ab219f82/TBBUwukEKX.lottie",
 ];
 
 function getRandomAnimationString(): string {
